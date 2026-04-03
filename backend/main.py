27051ee2fd1d570822e1db0770a67a3cad40bbc1
jlgut <jlgut@example.com>
2026-04-03 14:28:00 +0200

    fix: remove unused Request import from main.py

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-from fastapi import FastAPI, Request
+from fastapi import FastAPI
 from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
 
